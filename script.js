c338c702a34e7a9ca6b6465e6bc99d660c49718a
Yega Cho <yega1009@gmail.com>
2023-07-27 18:27:27 -0400

    created function for password length prompt

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Assignment code here
+function passwordLength() {
+  var length = prompt("Please choose a number between 8 to 128 (length for password)")
 
+  if (length > 128 || length < 8) {
+    alert("Try again. You must choose a number between 8 and 128")
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
