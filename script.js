7e8c9ec71912d412431e82fd8f5ee3088436953c
Yega Cho <yega1009@gmail.com>
2023-07-27 19:44:29 -0400

    added comments to javascript file

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// This function prompts user to choose length of password and decides on length of password
 function passwordLength() {
   var length = prompt("Please choose a number between 8 to 128 (length for password)");
 
+  // This if statement checks if chosen length is acceptable and if not, it will prompt user again
   if (length <= 128 && length >= 8) {
     return length;
   }
@@ -8,6 +10,7 @@ function passwordLength() {
   return passwordLength();
 }
 
+// This function prompts user to choose criteria and collects characters to include in the password
 function characterTypes() {
   var types = {
     lowercase: confirm("Would you like to include lowercase characters?"),
@@ -16,6 +19,7 @@ function characterTypes() {
     special: confirm("Would you like to include special characters?")
   };
 
+  // This if statement checks if user chose at least one character type and if not, prompts user again
   if (!types.lowercase && !types.uppercase && !types.numeric && !types.special) {
     alert("You must choose at least one character type");
     return characterTypes();
@@ -23,16 +27,20 @@ function characterTypes() {
   return types;
 }
 
+// This function creates randomized password
 function createMyPassword() {
+  // these variables call functions above for length of password and characters in password
   var length = passwordLength();
   var characterType = characterTypes();
   var combination = "";
   var password = "";
+  // These variables define the list of elements within each character type
   var charactersLower = "abcdefghijklmnopqrstuvwxyz";
   var charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var charactersNumeric = "0123456789";
   var charactersSpecial = "!#$$%&'()*,-./:;<=>?@[\]^_`{|}~";
 
+  // These if statements add different character types to the potential combination of password
   if (characterType.lowercase) {
     combination += charactersLower;
   }
@@ -46,6 +54,7 @@ function createMyPassword() {
     combination += charactersSpecial;
   }
 
+  // This for loop creates a randomized password with the selected length and character types
   for (var i = 0; i < length; i++) {
     password += combination[Math.floor(Math.random() * combination.length)];
   }
