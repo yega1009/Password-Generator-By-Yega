21fe3fedb0df23e2f4b51528912ea4f9e4e3e6d5
Yega Cho <yega1009@gmail.com>
2023-07-27 19:35:55 -0400

    created function to create password

--- script.js
+++ script.js
@@ -1,31 +1,64 @@
 function passwordLength() {
-  var length = prompt("Please choose a number between 8 to 128 (length for password)")
+  var length = prompt("Please choose a number between 8 to 128 (length for password)");
 
-  if (length > 128 || length < 8) {
-    alert("Try again. You must choose a number between 8 and 128")
+  if (length <= 128 && length >= 8) {
+    return length;
   }
+  alert("Try again. You must choose a number between 8 and 128");
+  return passwordLength();
 }
 
 function characterTypes() {
-  var lowercase = confirm("Would you like to include lowercase characters?")
-  var uppercase = confirm("Would you like to include uppercase characters?")
-  var numeric = confirm("Would you like to include numeric characters?")
-  var special = confirm("Would you like to include special characters?")
+  var types = {
+    lowercase: confirm("Would you like to include lowercase characters?"),
+    uppercase: confirm("Would you like to include uppercase characters?"),
+    numeric: confirm("Would you like to include numeric characters?"),
+    special: confirm("Would you like to include special characters?")
+  };
 
-  if (!lowercase && !uppercase && !numeric && !special) {
+  if (!types.lowercase && !types.uppercase && !types.numeric && !types.special) {
     alert("You must choose at least one character type");
+    return characterTypes();
   }
+  return types;
 }
 
+function createMyPassword() {
+  var length = passwordLength();
+  var characterType = characterTypes();
+  var combination = "";
+  var password = "";
+  var charactersLower = "abcdefghijklmnopqrstuvwxyz";
+  var charactersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var charactersNumeric = "0123456789";
+  var charactersSpecial = "!#$$%&'()*,-./:;<=>?@[\]^_`{|}~";
 
+  if (characterType.lowercase) {
+    combination += charactersLower;
+  }
+  if (characterType.uppercase) {
+    combination += charactersUpper;
+  }
+  if (characterType.numeric) {
+    combination += charactersNumeric;
+  }
+  if (characterType.special) {
+    combination += charactersSpecial;
+  }
 
+  for (var i = 0; i < length; i++) {
+    password += combination[Math.floor(Math.random() * combination.length)];
+  }
+
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = createMyPassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
